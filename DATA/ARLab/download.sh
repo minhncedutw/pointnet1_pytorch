314c2e862dbfb67369d7097b269e0f949714c3eb
minhncedutw <minhnc.edu.tw@gmail.com>
2018-09-01 09:24:04 +0700

    add download.sh for ARLab dataset

---
+++ DATA/ARLab/download.sh
@@ -0,0 +1,6 @@
+
+wget https://drive.google.com/open?id=1U2bY3yeiYM911h_73UdS2GVuOFHCVJat
+unzip allpts.zip
+rm shapenetcore_partanno_segmentation_benchmark_v0.zip
+mv trfedPTS points
+mv label points_label
