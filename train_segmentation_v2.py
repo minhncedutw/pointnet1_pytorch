996cd38da22283db13440a27984f014767d8717a
minhncedutw <minhnc.edu.tw@gmail.com>
2018-09-01 10:42:02 +0700

    create train segmentation using fastai lib

---
+++ train_segmentation_v2.py
@@ -0,0 +1,74 @@
+'''
+    File name: robot-grasping
+    Author: minhnc
+    Date created(MM/DD/YYYY): 9/1/2018
+    Last modified(MM/DD/YYYY HH:MM): 9/1/2018 9:25 AM
+    Python Version: 3.5
+    Other modules: [tensorflow-gpu 1.3.0]
+
+    Copyright = Copyright (C) 2017 of NGUYEN CONG MINH
+    Credits = [None] # people who reported bug fixes, made suggestions, etc. but did not actually write the code
+    License = None
+    Version = 0.9.0.1
+    Maintainer = [None]
+    Email = minhnc.edu.tw@gmail.com
+    Status = Prototype # "Prototype", "Development", or "Production"
+    Code Style: http://web.archive.org/web/20111010053227/http://jaynes.colorado.edu/PythonGuidelines.html#module_formatting
+'''
+
+#==============================================================================
+# Imported Modules
+#==============================================================================
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os.path
+import sys
+import time
+import random
+
+from fastai.conv_learner import *
+
+from DATA.ARLab.arlab_dataloader import PartDataset
+
+#==============================================================================
+# Constant Definitions
+#==============================================================================
+parser = argparse.ArgumentParser()
+parser.add_argument('--batchSize', type=int, default=8, help='input batch size')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=0) # Notice on Ubuntu, number worker should be 4; but on Windows, number worker HAVE TO be 0
+parser.add_argument('--nepoch', type=int, default=10, help='number of epochs to train for')
+parser.add_argument('--outf', type=str, default='DATA/ARLab/seg',  help='output folder')
+parser.add_argument('--model', type=str, default = '',  help='model path')
+
+opt = parser.parse_args()
+print (opt)
+
+opt.manualSeed = random.randint(1, 10000) # fix seed
+print("Random Seed: ", opt.manualSeed)
+random.seed(opt.manualSeed)
+torch.manual_seed(opt.manualSeed)
+
+#==============================================================================
+# Function Definitions
+#==============================================================================
+
+#==============================================================================
+# Main function
+#==============================================================================
+def main(argv=None):
+    print('Hello! This is XXXXXX Program')
+
+    num_points = 2700
+    dataset = PartDataset(root='DATA/ARLab/objects', npoints=num_points, classification=False, class_choice=['pipe'])
+    dataloader = DataLoader(dataset, batch_size=opt.batchSize, shuffle=False, num_workers=int(opt.workers))
+
+    for i, data in enumerate(dataloader.get_batch(0), 0):
+        points, target = data
+        break
+
+
+if __name__ == '__main__':
+    main()
