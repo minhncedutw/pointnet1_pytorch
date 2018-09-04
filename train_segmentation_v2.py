79ae110c64c0bbba3f84e26e1a10e0d9e18f4e59
minhncedutw <minhnc.edu.tw@gmail.com>
2018-09-04 10:55:34 +0700

    train_seg ver 2

--- train_segmentation_v2.py
+++ train_segmentation_v2.py
@@ -32,6 +32,7 @@ import random
 from fastai.conv_learner import *
 
 from DATA.ARLab.arlab_dataloader import PartDataset
+from pointnet import PointNetDenseCls
 
 #==============================================================================
 # Constant Definitions
@@ -63,18 +64,76 @@ def main(argv=None):
 
     num_points = 2048
     dataset = PartDataset(root='DATA/ARLab/objects', npoints=num_points, classification=False, class_choice=['pipe'])
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize, shuffle=False,
-                                             num_workers=int(opt.workers))
-    fastai_dataloader = DataLoader(dataset, batch_size=opt.batchSize, shuffle=False, num_workers=int(opt.workers))
+    # dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize, shuffle=False, num_workers=int(opt.workers))
+    dataloader = DataLoader(dataset, batch_size=opt.batchSize, shuffle=False, num_workers=int(opt.workers))
 
-    batch = next(iter(fastai_dataloader))
-    print(batch)
+    test_dataset = PartDataset(root='DATA/ARLab/objects', npoints=num_points, classification=False, class_choice=['pipe'])
+    testdataloader = torch.utils.data.DataLoader(test_dataset, batch_size=opt.batchSize, shuffle=True, num_workers=int(opt.workers))
 
-    for i, data in enumerate(fastai_dataloader, 0):
-        points, target = data
-        print(points.shape, target.shape)
-        break
+    num_classes = dataset.num_seg_classes
+
+    blue = lambda x:'\033[94m' + x + '\033[0m'
+
+    classifier = PointNetDenseCls(num_points=num_points, k=num_classes)
+
+    if opt.model != '':
+        classifier.load_state_dict(torch.load(opt.model))
+
+    optimizer = optim.SGD(classifier.parameters(), lr=0.01, momentum=0.9)
+    classifier.cuda()
+
+    num_batch = len(dataset) / opt.batchSize
+
+    for epoch in range(opt.nepoch):
+        for i, data in enumerate(dataloader, 0):
+            points, target = data
+            points, target = Variable(points), Variable(target)
+            points = points.transpose(2, 1)
+            points, target = points.cuda(), target.cuda()
+            optimizer.zero_grad()
+            pred, _ = classifier(points)
+            pred = pred.view(-1, num_classes)
+            target = target.view(-1, 1)[:, 0] - 1
+            # print(pred.size(), target.size())
+            loss = F.nll_loss(pred, target)
+            loss.backward()
+            optimizer.step()
+            pred_choice = pred.data.max(1)[1]
+            correct = pred_choice.eq(target.data).cpu().sum()
+            print('[%d: %d/%d] train loss: %f accuracy: %f' % (
+            epoch, i, num_batch, loss.item(), correct.item() / float(list(target.shape)[0])))
+
+            if i % 10 == 0:
+                j, data = next(enumerate(testdataloader, 0))
+                points, target = data
+                points, target = Variable(points), Variable(target)
+                points = points.transpose(2, 1)
+                points, target = points.cuda(), target.cuda()
+                pred, _ = classifier(points)
+                pred = pred.view(-1, num_classes)
+                target = target.view(-1, 1)[:, 0] - 1
+
+                loss = F.nll_loss(pred, target)
+                pred_choice = pred.data.max(1)[1]
+                correct = pred_choice.eq(target.data).cpu().sum()
+                print('[%d: %d/%d] %s loss: %f accuracy: %f' % (
+                epoch, i, num_batch, blue('test'), loss.item(), correct.item() / float(list(target.shape)[0])))
+
+        torch.save(classifier.state_dict(), '%s/seg_model_%d.pth' % (opt.outf, epoch))
 
 
 if __name__ == '__main__':
     main()
+
+'''
+    # dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize, shuffle=False, num_workers=int(opt.workers))
+    # dataloader = DataLoader(dataset, batch_size=opt.batchSize, shuffle=False, num_workers=int(opt.workers))
+    
+    batch = next(iter(dataloader))
+    print(batch)
+
+    for i, data in enumerate(dataloader, 0):
+        points, target = data
+        print(points.shape, target.shape)
+        break
+'''
